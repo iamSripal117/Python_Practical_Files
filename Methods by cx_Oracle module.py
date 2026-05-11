6de9b2aa42c361433f45768be66e93617333901a
Sripal Rakkireddy <sripalrakkireddy1817@gmail.com>
2026-05-11 22:11:18 +0530

    New Files

---
+++ Methods by cx_Oracle module.py
@@ -0,0 +1,14 @@
+# following are the methods provided by cx_module, which can be used for database programming
+
+# connect()
+# cursor()
+# execute()
+# executescript()
+# executemany()
+# close()
+# commit()
+# rollback()
+# fetch()
+# fetchone()
+# fetchall()
+# fetchmany()
